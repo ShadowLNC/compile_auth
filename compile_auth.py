155145cb5427d6a6c3f921ab9eae63a4953034a8
Scott Stevens <scott.stevens@roboroos.org.au>
2016-02-02 19:11:12 +1030

    Changed to recurse through individual_keys folder; now uses file location and not execution location
    Using all files in individual_keys as a key (os.walk vs os.listdir)
    
    Getting the file location from http://stackoverflow.com/questions/3718657/how-to-properly-determine-current-script-directory-in-python
    As noted, exec() calls will stuff this up

--- compile_auth.py
+++ compile_auth.py
@@ -1,6 +1,8 @@
-import os,sys
-os.chdir(sys.path[0])
-with open("authorized_keys", "w") as authfile:
-	for fn in os.listdir("individual_keys"):
-		with open("individual_keys/"+fn) as file:
-			authfile.write(file.read())
+from os import path, walk
+wd = path.dirname(path.abspath(__file__)) #working directory
+with open(path.join(wd,"../authorized_keys"), "w") as authfile:
+    #using authfile to store all
+    for dirpath,dirnames,filenames in walk(path.join(wd,"individual_keys")):
+        for f in filenames:
+            with open(path.join(dirpath,f)) as file:
+                authfile.write(file.read()+"\n")
