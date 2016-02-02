afccaa8fb6257cd44dee4cc988bc4082111bc16b
Scott Stevens <scott.stevens@roboroos.org.au>
2016-02-03 01:26:06 +1030

    Added config parser (optional) for modularity
    "config" file should be put either in repository root or parent
    Recognised properties are authfile (usually ../authorized_keys) and keyfolder (usually individual_keys)
    Added structure for Google Drive "pull" support, but not yet complete

--- compile_auth.py
+++ compile_auth.py
@@ -1,8 +1,20 @@
-from os import path, walk
-wd = path.dirname(path.abspath(__file__)) #working directory
-with open(path.join(wd,"../authorized_keys"), "w") as authfile:
+import os
+wd = os.path.dirname(os.path.abspath(__file__)) #working directory
+os.chdir(wd)
+
+from configparser import ConfigParser
+config = ConfigParser(
+    {"authfile": "../authorised_keys", "keyfolder": "individual_keys"})
+config.read(["config", "../config"]) #try either spot
+
+if "GDRIVE" in config:
+    #we have a drive configuration and should use it
+    pass #todo: implement
+
+mainCFG = config["DEFAULT"]
+with open(mainCFG.get("authfile"), "w") as authfile:
     #using authfile to store all
-    for dirpath,dirnames,filenames in walk(path.join(wd,"individual_keys")):
+    for dirpath,dirnames,filenames in os.walk(mainCFG.get("keyfolder")):
         for f in filenames:
-            with open(path.join(dirpath,f)) as file:
+            with open(os.path.join(dirpath,f)) as file:
                 authfile.write(file.read()+"\n")
