292405effc5486e61654b369c91cee846af5672a
Scott Stevens <scott.stevens@roboroos.org.au>
2016-02-04 15:17:25 +1030

    Cleaning up user file paths - **Relative to compile_auth.py** - default to repository parent folder
    Includes individual_keys and compile_auth.cfg; .gitignore has been updated to reflect these potential files.
    
    A note on the config file: Either config or compile_auth.cfg will be searched in the repository, but only the latter in the parent folder.
    Note that since the previous commit which introduced ../config was not yet pushed, this is inconsequential.

--- compile_auth.py
+++ compile_auth.py
@@ -4,17 +4,21 @@ os.chdir(wd)
 
 from configparser import ConfigParser
 config = ConfigParser(
-    {"authfile": "../authorised_keys", "keyfolder": "individual_keys"})
-config.read(["config", "../config"]) #try either spot
+    {"authfile": "../authorised_keys", "keyfolder": "../individual_keys"})
+#try multiple config locations, preferred ../compile_auth.cfg
+#note all paths are relative to this file!
+config.read(["config", "compile_auth.cfg", "../compile_auth.cfg"])
+mainCFG = config["DEFAULT"]
 
 if "GDRIVE" in config:
     #we have a drive configuration and should use it
     pass #todo: implement
 
-mainCFG = config["DEFAULT"]
 with open(mainCFG.get("authfile"), "w") as authfile:
-    #using authfile to store all
     for dirpath,dirnames,filenames in os.walk(mainCFG.get("keyfolder")):
+        
+        #foreach file
         for f in filenames:
             with open(os.path.join(dirpath,f)) as file:
-                authfile.write(file.read()+"\n")
+                authfile.write(file.read()+"\n") #using authfile to store all
+
